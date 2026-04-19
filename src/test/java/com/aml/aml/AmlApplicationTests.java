7b1fd98fcce2643028395f3205930b11fe0842e2
OmKariya-TSS <om.kariya@tssconsultancy.com>
2026-04-19 15:05:11 +0530

    initialized backend springboot folder

---
+++ src/test/java/com/aml/aml/AmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aml.aml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
