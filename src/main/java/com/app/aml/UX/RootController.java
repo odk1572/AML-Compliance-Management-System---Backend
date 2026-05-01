25cbda7d9d035861a74acbecb022b774541b6e50
Om Kariya <161995427+odk1572@users.noreply.github.com>
2026-05-01 09:22:01 +0530

    Create RootController to handle root requests

---
+++ src/main/java/com/app/aml/UX/RootController.java
@@ -0,0 +1,12 @@
+package com.app.aml.UX;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public String home() {
+        return "AML Backend is Running Successfully!";
+    }
+}
